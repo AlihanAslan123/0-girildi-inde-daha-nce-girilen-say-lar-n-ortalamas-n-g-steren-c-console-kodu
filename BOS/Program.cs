75023409fe13bccecc02a9d1f13019acb45115c7
AlihanAslan123 <155809854+AlihanAslan123@users.noreply.github.com>
2024-02-03 23:06:30 +0300

    Proje dosyası ekle.

---
+++ BOS/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int sayi;
+             int sayac = 0;
+             double toplam = 0;
+             double ort = 0;
+             while (true)
+             {
+                 Console.Write("sayı giriniz: ");
+                 sayi = int.Parse(Console.ReadLine());
+                 toplam += sayi;
+                 sayac++;
+                 if (sayi == 0)
+                 {
+                     goto a;
+                 }
+
+             }
+
+             a:
+             ort = toplam / sayac;
+             Console.WriteLine("Girilen sayıların ortalaması = "+ort);
+             Console.ReadLine(); 
+
+        }
+    }
+}
